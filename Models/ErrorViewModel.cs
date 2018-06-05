10e9e820cb987da7ed070327964d89971b4ac2c3
Fagun Rain <fagunrain@Faguns-MacBook-Pro.local>
2018-06-06 01:12:37 +0600

    Initial project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OWIN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
